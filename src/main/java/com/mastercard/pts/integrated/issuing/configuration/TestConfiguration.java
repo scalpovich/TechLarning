c140f82cb03ef9a5e64f3ac256c85cfdd8d47700
Ravi Kukreja <ravi_kukreja@mastercard.com>
2017-12-28 16:07:49 +0530

    Formatting changes

--- src/main/java/com/mastercard/pts/integrated/issuing/configuration/TestConfiguration.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/configuration/TestConfiguration.java
@@ -23,18 +23,14 @@ import com.mastercard.testing.utils.encryption.EncryptUtils;
 
 @Configuration
 @ComponentScan(basePackages = { "com.mastercard.pts.integrated.issuing" })
-@PropertySources({
-		@PropertySource(value = "/config/${env}/environment.properties", ignoreResourceNotFound = false),
-		@PropertySource(value = "/config/${env}/user.properties", ignoreResourceNotFound = false),
-		@PropertySource(value = "/config/${env}/test.properties", ignoreResourceNotFound = false),
-		@PropertySource(value = "/config/${env}/apitest.properties", ignoreResourceNotFound = false),
-		@PropertySource(value = "/config/${env}/validation.properties", ignoreResourceNotFound = false), })
+@PropertySources({ @PropertySource(value = "/config/${env}/environment.properties", ignoreResourceNotFound = false),
+		@PropertySource(value = "/config/${env}/user.properties", ignoreResourceNotFound = false), @PropertySource(value = "/config/${env}/test.properties", ignoreResourceNotFound = false),
+		@PropertySource(value = "/config/${env}/apitest.properties", ignoreResourceNotFound = false), @PropertySource(value = "/config/${env}/validation.properties", ignoreResourceNotFound = false), })
 @Import({ MTAFWebToolsConfiguration.class, Portal.class })
 @EnableAspectJAutoProxy
 public class TestConfiguration {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(TestConfiguration.class);
+	private static final Logger logger = LoggerFactory.getLogger(TestConfiguration.class);
 
 	private static final String CHROME = "chrome";
 
@@ -47,17 +43,13 @@ public class TestConfiguration {
 	public Path tempDirectory() {
 		Path temp = null;
 		try {
-			temp = Files.createTempDirectory(new DateUtils().getDateyyyyMMdd()
-					+ "_IssuingTests_");
+			temp = Files.createTempDirectory(new DateUtils().getDateyyyyMMdd() + "_IssuingTests_");
 			if (System.getProperty(PLATFORM).equalsIgnoreCase(CHROME)) {
-				Runtime.getRuntime()
-						.exec("cmd /c start \"\" src\\main\\resources\\config\\killChromeDriver.bat");
+				Runtime.getRuntime().exec("cmd /c start \"\" src\\main\\resources\\config\\killChromeDriver.bat");
 				logger.info("Killed all chromedriver.exe instances");
 			}
 		} catch (Exception e) {
-			logger.error(
-					"Exception has been caugh while creating a temp folder or while killing chrome driver instances.\nMessage :: {}\nTrace :: {}",
-					e.getMessage(), e.getStackTrace());
+			logger.error("Exception has been caugh while creating a temp folder or while killing chrome driver instances.\nMessage :: {}\nTrace :: {}", e.getMessage(), e.getStackTrace());
 			MiscUtils.propagate(e);
 		}
 		logger.info("Temp directory path: {}", temp);
@@ -92,35 +84,24 @@ public class TestConfiguration {
 		Portal portal = new Portal();
 		portal.setUrl(getPortalProperty(portalType, "url"));
 		if (portalType.equalsIgnoreCase(Portal.TYPE_AGENT)) {
-			portal.setAdminUserName(getPortalProperty(portalType,
-					"user.admin.name"));
-			portal.setAgencyUserName(getPortalProperty(portalType,
-					"user.agency.name"));
-			portal.setBranchUserName(getPortalProperty(portalType,
-					"user.branch.name"));
-			portal.setAgentUserName(getPortalProperty(portalType,
-					"user.agent.name"));
-			portal.setNonfundedAgencyUserName(getPortalProperty(portalType,
-					"user.nonfundedagency.name"));
-			portal.setNonfundedBranchUserName(getPortalProperty(portalType,
-					"user.nonfundedbranch.name"));
-			portal.setNonfundedAgentUserName(getPortalProperty(portalType,
-					"user.nonfundedagent.name"));
+			portal.setAdminUserName(getPortalProperty(portalType, "user.admin.name"));
+			portal.setAgencyUserName(getPortalProperty(portalType, "user.agency.name"));
+			portal.setBranchUserName(getPortalProperty(portalType, "user.branch.name"));
+			portal.setAgentUserName(getPortalProperty(portalType, "user.agent.name"));
+			portal.setNonfundedAgencyUserName(getPortalProperty(portalType, "user.nonfundedagency.name"));
+			portal.setNonfundedBranchUserName(getPortalProperty(portalType, "user.nonfundedbranch.name"));
+			portal.setNonfundedAgentUserName(getPortalProperty(portalType, "user.nonfundedagent.name"));
 		} else {
 			portal.setUserName(getPortalProperty(portalType, "user.name"));
-			portal.setAdminUserName(getPortalProperty(portalType,
-					"user.admin.name"));
+			portal.setAdminUserName(getPortalProperty(portalType, "user.admin.name"));
 		}
 		String password;
 		String adminPassword;
 		try {
-			password = EncryptUtils.decrypt(getPortalProperty(portalType,
-					"user.password"));
-			adminPassword = EncryptUtils.decrypt(getPortalProperty(portalType,
-					"user.admin.password"));
+			password = EncryptUtils.decrypt(getPortalProperty(portalType, "user.password"));
+			adminPassword = EncryptUtils.decrypt(getPortalProperty(portalType, "user.admin.password"));
 		} catch (Exception e) {
-			logger.error("Fail to decrypt password for user {}",
-					portal.getUserName());
+			logger.error("Fail to decrypt password for user {}", portal.getUserName());
 			MiscUtils.propagate(e);
 			return null;
 		}
@@ -130,8 +111,7 @@ public class TestConfiguration {
 	}
 
 	private String getPortalProperty(String portalType, String propertyName) {
-		String fullProperty = String.format("portal.%s.%s", portalType,
-				propertyName);
+		String fullProperty = String.format("portal.%s.%s", portalType, propertyName);
 		return environment.getProperty(fullProperty);
 	}
 }
