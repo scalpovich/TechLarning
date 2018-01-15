6df63d458f0df27c2effd9815a94bb6f57c1ff3b
Lulla <Lokesh.Lulla@mastercard.com>
2018-01-15 19:30:17 +0530

    Added code for RestAssured API Automation<WIP>

--- src/main/java/com/mastercard/pts/integrated/issuing/domain/restapi/DeviceDetails.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/domain/restapi/DeviceDetails.java
@@ -1,24 +1,203 @@
 package com.mastercard.pts.integrated.issuing.domain.restapi;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Assert;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+import com.mastercard.pts.integrated.issuing.steps.restapi.RestStepDefinitions;
+import com.mastercard.pts.integrated.issuing.utils.ConnectionUtils;
 
 @Component
 public class DeviceDetails {
-	
+
+	@Autowired
+	ConnectionUtils connectionUtils;
+	// Queries for CardActivation API
+	public static final String GETCPICRDQUERY = "SELECT DEVICE_NUMBER ,KYC_STATUS ,CARD_PACK_ID FROM device INNER JOIN client ON device.CLIENT_CODE = client.CLIENT_CODE INNER JOIN device_plan on device.DEVICE_PLAN_CODE = device_plan.DEVICE_PLAN_CODE where KYC_STATUS='Y' AND device.bank_code='111777' AND device.Status_code=0 AND ACTIVATION_DATE is null AND device.DELIVERY_MODE is null AND DELIVERY_DEFAULT LIKE '____1%' AND ROWNUM=1";
+	public static final String GETEXPIREDCARDQUERY = "SELECT DEVICE_NUMBER,CARD_PACK_ID from device INNER JOIN device_plan on device.DEVICE_PLAN_CODE = device_plan.DEVICE_PLAN_CODE where device.STATUS_CODE=12 and device.bank_code='111777' and device.EXPIRY_DATE is NOT NULL and device_plan.DEVICE_TYPE not IN (7,8) and device.ACTIVATION_DATE is null AND ROWNUM=1";
+	public static final String GETCANCELLEDCARDQUERY = "SELECT Status_code,DEVICE_NUMBER,CARD_PACK_ID,DEVICE_TYPE from device INNER JOIN device_plan on device.DEVICE_PLAN_CODE = device_plan.DEVICE_PLAN_CODE where STATUS_CODE=3 and device.bank_code='111777' and ACTIVATION_DATE is null and device_plan.DEVICE_TYPE not IN (7,8) AND ROWNUM=1";
+	public static final String GETSTOPLISTEDCARDQUERY = "SELECT device.DELIVERY_MODE,device.ACTIVATION_DATE,DEVICE_PLAN.DELIVERY_DEFAULT,device.Status_code,device.DEVICE_NUMBER,device.CARD_PACK_ID,device_plan.DEVICE_TYPE,device.bank_code from device INNER JOIN client ON device.CLIENT_CODE = client.CLIENT_CODE INNER JOIN device_plan on device.DEVICE_PLAN_CODE = device_plan.DEVICE_PLAN_CODE where device.BANK_CODE='111777' and device_plan.DEVICE_TYPE not IN (7,8) and  device.ACTIVATION_DATE is null and device_plan.DELIVERY_DEFAULT LIKE '____1%' and device.Status_code in (8,19) and device.DELIVERY_MODE is not null and ROWNUM=1";
+	public static final String GETACTIVATEDCARDQUERY = "SELECT DEVICE_NUMBER,CARD_PACK_ID FROM device INNER JOIN client ON device.CLIENT_CODE = client.CLIENT_CODE INNER JOIN device_plan on device.DEVICE_PLAN_CODE = device_plan.DEVICE_PLAN_CODE where KYC_STATUS='Y' AND device.bank_code='111777' AND device.Status_code=0 AND ACTIVATION_DATE is not null AND device.DELIVERY_MODE is null AND DELIVERY_DEFAULT ='000010000' AND DELIVERY_DATE IS NOT NULL AND DELIVERY_FLAG NOT IN (0) and ROWNUM=1";
+	// Queries for Card link delink API
+	public static final String GETDEVICE = "select PRODUCT_TYPE,DEVICE_NUMBER,DEFAULT_WALLET_NUMBER,WALLET_CUSTOM_NUMBER1,WALLET_CUSTOM_NUMBER2 from device INNER JOIN client ON device.CLIENT_CODE = client.CLIENT_CODE where device.bank_code='111777' and device.ACTIVATION_DATE is not null and device.PRODUCT_TYPE='D' and RowNum=1";
+	public static final String GETCLIENTLENGTH = "select CLIENT_LEN from Institution where bank_code='111777'";
+	public static final String GETCARDLINKSTATUS = "select DEVICE_NUMBER,WALLET_NUMBER,DEFAULT_WALLET_FLAG,ACCOUNT_TYPE,PRODUCT_TYPE,BANK_CODE from DEVICE_WALLET_LINK WHERE DEVICE_NUMBER='?'";
+
+	// Device Table cloumnNames
+	public static final String DEVICENUMBERCOLUMN = "DEVICE_NUMBER";
+	public static final String CARDPACKIDCOLUMN = "CARD_PACK_ID";
+
+	// Institution Table cloumnNames
+	public static final String CLIENTLENGTHCOLUMN = "CLIENT_LEN";
+	// DEVICE_WALLET_LINK Table cloumnNames
+	public static final String DEFAULTWALLETFLAGCOLUMN = "DEFAULT_WALLET_FLAG";
+	public static final String WALLETNUMBERCOLUMN = "WALLET_NUMBER";
+	ResultSet result;
+	final Logger logger = LoggerFactory.getLogger(DeviceDetails.class);
+
 	private String transactionId;
-	private	String timeStamp;
+	private String timeStamp;
+	private String deviceNumber;
+	private String defaultWallet;
+	private String customWallet;
+
+	public String getDefaultWallet() {
+		return defaultWallet;
+	}
+
+	public void setDefaultWallet(String defaultWallet) {
+		this.defaultWallet = defaultWallet;
+	}
+
+	public String getCustomWallet() {
+		return customWallet;
+	}
+
+	public void setCustomWallet(String customWallet) {
+		this.customWallet = customWallet;
+	}
+
+	private String cardPackID;
+	private String statusCode;
+
+	public void getCPICRDDataFromDB() {
+		result = connectionUtils.executeQueryForBIN(GETCPICRDQUERY);
+		try {
+			while (result.next()) {
+				setCardPackID(result.getString(CARDPACKIDCOLUMN));
+				setDeviceNumber(result.getString(DEVICENUMBERCOLUMN));
+			}
+		} catch (Exception e) {
+			logger.error("Error Message : ", e);
+		}
+	}
+
+	public void getCardStatusFromDB(String query) {
+		result = connectionUtils.executeQueryForBIN(query);
+		try {
+			while (result.next()) {
+				setDeviceNumber(result.getString("DEVICE_NUMBER"));
+			}
+		} catch (Exception e) {
+			logger.error("Error Message", e);
+		}
+	}
+
+	public String getDataFromDB(String query, String coulmn) {
+		String data = null;
+		result = connectionUtils.executeQueryForBIN(query);
+		try {
+			while (result.next()) {
+				data = result.getString(coulmn);
+			}
+		} catch (Exception e) {
+			logger.error("Error Message", e);
+		}
+		return data;
+	}
+
+	public void checkWalletLinkStatus() {
+		try {
+			result = connectionUtils
+					.executeQueryForBIN(GETCARDLINKSTATUS.replace("?",
+							getDeviceNumber()));
+
+			while (result.next()) {
+				if (result.getString(WALLETNUMBERCOLUMN).equals(
+						getDefaultWallet())) {
+					logger.info("****************Match Found************************\n WALLET NUMBER COLUMN="
+							+ result.getString(WALLETNUMBERCOLUMN));
+					Assert.assertTrue(result.getString(DEFAULTWALLETFLAGCOLUMN)
+							.equals("Y"));
+				} else if (result.getString(WALLETNUMBERCOLUMN).equals(
+						getCustomWallet())) {
+					logger.info("****************Match Found************************\n WALLET NUMBER COLUMN="
+							+ result.getString(WALLETNUMBERCOLUMN));
+					Assert.assertTrue(result.getString(DEFAULTWALLETFLAGCOLUMN)
+							.equals("N"));
+				} else {
+					logger.info("****************NO Match ************************");
+
+				}
+			}
+		} catch (Exception e) {
+			logger.error("Error Message ", e);
+		}
+
+	}
+
+	public void checkWalletDelinkStatus() {
+		try {
+			result = connectionUtils
+					.executeQueryForBIN(GETCARDLINKSTATUS.replace("?",
+							getDeviceNumber()));
+
+			while (result.next()) {
+				if (result.getString(WALLETNUMBERCOLUMN).equals(
+						getDefaultWallet())) {
+					logger.info("****************Match Found************************\n DEFAULTWALLETFLAGCOLUMN="
+							+ result.getString(WALLETNUMBERCOLUMN));
+					Assert.assertTrue(result.getString(DEFAULTWALLETFLAGCOLUMN)
+							.equals("N"));
+				} else if (result.getString(WALLETNUMBERCOLUMN).equals(
+						getCustomWallet())) {
+					logger.info("****************Match Found************************\n DEFAULTWALLETFLAGCOLUMN="
+							+ result.getString(WALLETNUMBERCOLUMN));
+					Assert.assertTrue(result.getString(DEFAULTWALLETFLAGCOLUMN)
+							.equals("Y"));
+				} else {
+					logger.info("****************NO Match ************************");
+				}
+			}
+		} catch (Exception e) {
+			logger.error("ERROR Message", e);
+		}
+
+	}
+
+	public String getDeviceNumber() {
+		return deviceNumber;
+	}
+
+	public void setDeviceNumber(String deviceNumber) {
+		this.deviceNumber = deviceNumber;
+	}
+
+	public String getCardPackID() {
+		return cardPackID;
+	}
+
+	public void setCardPackID(String cardPackID) {
+		this.cardPackID = cardPackID;
+	}
+
 	public String getTransactionId() {
 		return transactionId;
 	}
+
 	public void setTransactionId(String transactionId) {
 		this.transactionId = transactionId;
 	}
+
 	public String getTimeStamp() {
 		return timeStamp;
 	}
+
 	public void setTimeStamp(String timeStamp) {
 		this.timeStamp = timeStamp;
 	}
-	
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
 
 }
--- src/main/java/com/mastercard/pts/integrated/issuing/utils/restapi/JsonUtil.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/utils/restapi/JsonUtil.java
@@ -4,13 +4,10 @@ import static com.jayway.restassured.RestAssured.given;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-
 import static org.hamcrest.Matchers.*;
 
 import org.codehaus.jackson.map.ObjectMapper;
@@ -18,20 +15,31 @@ import org.codehaus.jackson.type.TypeReference;
 import org.jbehave.core.model.ExamplesTable;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
+import com.mastercard.pts.integrated.issuing.domain.restapi.DeviceDetails;
 import com.mastercard.pts.integrated.issuing.steps.AbstractBaseSteps;
+import com.mastercard.pts.integrated.issuing.utils.CustomUtils;
 
 @Component
 public class JsonUtil extends AbstractBaseSteps {
 	final Logger logger = LoggerFactory.getLogger(JsonUtil.class);
 	public static final String REQ_JSON_FILE_PATH = "src\\main\\resources\\webServiceRequestJsons\\";
-	public static final String END_POINT = "/users";
+	public static final String DN_KEY = "X-Client-DN";
+	public static final String DEMO_CARDACTIVATION_DN_VALUE = "CN=virgo01.mi.dev.mastercard.int,OU=Server 00,O=Mastercard Worldwide,L=St Louis,S=Missouri,C=US";
+	public static final String STAGE_CARDACTIVATION_DN_VALUE = "CN=Airtel-Stage,OU=sa-stage-internalclient,O=MasterCard Worldwide - Corp Netw Infra,L=Saint Louis,ST=Missouri,C=US";
+	public static final String CARD_ACTIVATION_END_POINT = "/cardactivation";
+	public static final String CARD_LINK_END_POINT = "/wallet?action=link";
+	public static final String CARD_DELINK_END_POINT = "/wallet?action=delink";
+	public static final int STATUS_CODE_SUCCESS = 200;
+	public static final String STATUS_CODELINE_SUCCESS = "ok";
 	File jsonFile;
 	ObjectMapper mapper;
 	ArrayList<Map<String, Object>> reqMapObject;
@@ -43,6 +51,11 @@ public class JsonUtil extends AbstractBaseSteps {
 	JSONObject jsonObject;
 	JSONParser parser;
 	Object obj;
+
+	@Autowired
+	DeviceDetails deviceDetails;
+	@Autowired
+	RestAssuredConfiguration restAssuredConfiguration;
 
 	public List<Map<String, Object>> parseToListMap(String jsonFilePath) {
 		mapper = new ObjectMapper();
@@ -89,8 +102,36 @@ public class JsonUtil extends AbstractBaseSteps {
 	public Response postRequest(String reqObject) {
 		RestAssured.baseURI = env.getProperty("api.base.uri");
 		response = given().body(reqObject).when().contentType(ContentType.JSON)
-				.post(END_POINT);
-		logger.info("Response String:"+response.asString());
+				.post(CARD_ACTIVATION_END_POINT);
+		logger.info("**********Request String:" + reqObject.toString());
+		logger.info("**********Response String:"
+				+ response.asString().toString());
+		return response;
+
+	}
+
+	public Response putRequest(String reqObject) {
+		RestAssured.config = restAssuredConfiguration.getRestAssuredConfig();
+		RestAssured.baseURI = restAssuredConfiguration.getbaseUrl();
+
+		response = given().header(DN_KEY, STAGE_CARDACTIVATION_DN_VALUE)
+				.body(reqObject).when().contentType(ContentType.JSON).put("");
+		logger.info("**********Request String:" + reqObject.toString());
+		logger.info("**********Response String:"
+				+ response.asString().toString());
+		return response;
+
+	}
+
+	public Response putRequest(String reqObject, String parameter) {
+		RestAssured.config = restAssuredConfiguration.getRestAssuredConfig();
+		RestAssured.baseURI = restAssuredConfiguration.getbaseUrl();
+		response = given().header(DN_KEY, STAGE_CARDACTIVATION_DN_VALUE)
+				.parameters("action", parameter).body(reqObject).when()
+				.contentType(ContentType.JSON).put("");
+		logger.info("**********Request String:" + reqObject.toString());
+		logger.info("**********Response String:"
+				+ response.asString().toString());
 		return response;
 
 	}
@@ -102,8 +143,15 @@ public class JsonUtil extends AbstractBaseSteps {
 			String actualData = input.split("=")[0];
 			String expectedData = input.split("=")[1];
 			response.then().body(actualData, equalTo(expectedData));
+			logger.info("Status Code" + response.getStatusCode());
+			logger.info("Status Line" + response.getStatusLine());
+
 		}
 
+	}
+
+	public void validateStatusCode(int statusCode, Response response) {
+		Assert.assertEquals(statusCode, response.getStatusCode());
 	}
 
 	public void validateResponse(Response response, String input) {
@@ -154,14 +202,14 @@ public class JsonUtil extends AbstractBaseSteps {
 			params = inputArray[0].split(",");
 			String[] expectedKeyValue = inputArray[1].split("=");
 			updateKey = expectedKeyValue[0];
-			updateValue = expectedKeyValue.length > 1 ? expectedKeyValue[1]
-					: null;
+			updateValue = getData(expectedKeyValue.length > 1 ? expectedKeyValue[1]
+					: null);
 
 		} else {
 			String[] expectedKeyValue = input.split("=");
 			updateKey = expectedKeyValue[0];
-			updateValue = expectedKeyValue.length > 1 ? expectedKeyValue[1]
-					: null;
+			updateValue = getData(expectedKeyValue.length > 1 ? expectedKeyValue[1]
+					: null);
 
 		}
 
@@ -202,6 +250,73 @@ public class JsonUtil extends AbstractBaseSteps {
 
 	}
 
+	// To get the data From DB based on key passed from story file
+	public String getData(String value) {
+		String valueToUpdate = value;
+		switch (valueToUpdate) {
+		case "ActivateCPI":
+			deviceDetails.getCPICRDDataFromDB();
+			valueToUpdate = deviceDetails.getCardPackID();
+			break;
+		case "ActivateCRD":
+			deviceDetails.getCPICRDDataFromDB();
+			valueToUpdate = deviceDetails.getDeviceNumber();
+			break;
+		case "ActivatedCRD":
+			deviceDetails
+					.getCardStatusFromDB(DeviceDetails.GETACTIVATEDCARDQUERY);
+			valueToUpdate = deviceDetails.getDeviceNumber();
+			break;
+		case "Cancelled":
+			deviceDetails
+					.getCardStatusFromDB(DeviceDetails.GETCANCELLEDCARDQUERY);
+			valueToUpdate = deviceDetails.getDeviceNumber();
+			break;
+		case "Expired":
+			deviceDetails
+					.getCardStatusFromDB(DeviceDetails.GETEXPIREDCARDQUERY);
+			valueToUpdate = deviceDetails.getDeviceNumber();
+			break;
+		case "Stoplisted":
+			deviceDetails
+					.getCardStatusFromDB(DeviceDetails.GETSTOPLISTEDCARDQUERY);
+			valueToUpdate = deviceDetails.getDeviceNumber();
+			break;
+		case "GetDevice":
+			valueToUpdate = deviceDetails.getDataFromDB(
+					DeviceDetails.GETDEVICE, DeviceDetails.DEVICENUMBERCOLUMN);
+			deviceDetails.setDeviceNumber(valueToUpdate);
+			break;
+		case "GenrateDefaultWalletNum":
+			valueToUpdate = CustomUtils.randomNumbers(Integer
+					.parseInt(deviceDetails.getDataFromDB(
+							DeviceDetails.GETCLIENTLENGTH,
+							DeviceDetails.CLIENTLENGTHCOLUMN)));
+			deviceDetails.setDefaultWallet(valueToUpdate);
+			break;
+		case "GenrateCustomWalletNum":
+			valueToUpdate = CustomUtils.randomNumbers(Integer
+					.parseInt(deviceDetails.getDataFromDB(
+							DeviceDetails.GETCLIENTLENGTH,
+							DeviceDetails.CLIENTLENGTHCOLUMN)));
+			deviceDetails.setCustomWallet(valueToUpdate);
+			break;
+		case "GetDeviceDOM":
+			valueToUpdate = deviceDetails.getDeviceNumber();
+			break;
+		case "GetDefaultWalletDOM":
+			valueToUpdate = deviceDetails.getDefaultWallet();
+			break;
+		case "GetCustomWalletDOM":
+			valueToUpdate = deviceDetails.getCustomWallet();
+			break;
+		default:
+
+		}
+
+		return valueToUpdate;
+	}
+
 	public String updateJasonFileWithAttributes(String filepath,
 			ExamplesTable attributeTable) {
 		LinkedList<JSONObject> tempJdata = new LinkedList<>();
@@ -224,14 +339,14 @@ public class JsonUtil extends AbstractBaseSteps {
 				params = inputArray[0].split(",");
 				String[] expectedKeyValue = inputArray[1].split("=");
 				updateKey = expectedKeyValue[0];
-				updateValue = expectedKeyValue.length > 1 ? expectedKeyValue[1]
-						: null;
+				updateValue = getData(expectedKeyValue.length > 1 ? expectedKeyValue[1]
+						: null);
 
 			} else {
 				String[] expectedKeyValue = input.split("=");
 				updateKey = expectedKeyValue[0];
-				updateValue = expectedKeyValue.length > 1 ? expectedKeyValue[1]
-						: null;
+				updateValue = getData(expectedKeyValue.length > 1 ? expectedKeyValue[1]
+						: null);
 
 			}
 
