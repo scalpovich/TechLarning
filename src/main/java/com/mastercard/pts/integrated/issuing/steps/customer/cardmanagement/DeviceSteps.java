a870a002bf9e68a7921db5a1ccc3c265225c2206
Ravi Kukreja <ravi_kukreja@mastercard.com>
2018-02-01 19:40:40 +0530

    SyncFixes

--- src/main/java/com/mastercard/pts/integrated/issuing/steps/customer/cardmanagement/DeviceSteps.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/steps/customer/cardmanagement/DeviceSteps.java
@@ -5,7 +5,6 @@ import org.jbehave.core.annotations.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.jcabi.log.Logger;
 import com.mastercard.pts.integrated.issuing.context.ContextConstants;
 import com.mastercard.pts.integrated.issuing.context.TestContext;
 import com.mastercard.pts.integrated.issuing.domain.ProductType;
