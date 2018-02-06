c4c6b1e4311f345ba17c6584ec8eb88fd1358227
Lokesh Lulla <E076170@GH-BMBGG32.corp.mastercard.org>
2018-02-06 18:56:31 +0530

    change steps for auth search

--- src/main/java/com/mastercard/pts/integrated/issuing/steps/customer/cardmanagement/AuthorizationSearchSteps.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/steps/customer/cardmanagement/AuthorizationSearchSteps.java
@@ -28,9 +28,9 @@ public class AuthorizationSearchSteps {
 	public void verifyBillingCurrency(String tcurrency, String bcurrency) {
 		Device device = context.get(ContextConstants.DEVICE);
 		authorizationSearchWorkflow.verifyTransactionAndBillingCurrency(tcurrency, bcurrency, device.getDeviceNumber());
-	}
+	}	
 	
-	@Then("validate auth report")
+@Then("validate auth report")
 	public void validateAuthReport() {
 		Device device = context.get(ContextConstants.DEVICE);
 		authorizationSearchWorkflow.verifyAuthTransactionSearchReport(device);
