a2bff953f2542a201803b6e3178b2d0d9b23d9db
Ramaswamy, Prabhu <Prabhu.Ramaswamy@mastercard.com>
2018-02-14 10:05:32 +0530

    Generate Batch Device is taking more time.. not coming up immediately as soon as we click it hence this code change to click if button is visible

--- src/main/java/com/mastercard/pts/integrated/issuing/pages/customer/cardmanagement/DeviceCreateDevicePage.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/pages/customer/cardmanagement/DeviceCreateDevicePage.java
@@ -248,6 +248,16 @@ public class DeviceCreateDevicePage extends AbstractBasePage {
 		WebElementUtils.selectDropDownByVisibleText(createOpenBatchDDwn, device.getCreateOpenBatch());
 		generateDeviceBatchBtn.click();
 		waitForWicket();
+		 // sometimes the Number is getting genarated very late hence the extra wait time
+		int c = 0;
+		if(generateDeviceBatchBtn.isVisible()) {
+			generateDeviceBatchBtn.click();
+			SimulatorUtilities.wait(2000);
+			c++;
+			if(c>5)
+			return;
+		}
+		waitForWicket();
 		// fetching batch number and setting it for further use
 		device.setBatchNumber(batchNumberTxt.getText());
 		logger.info(" *********** Batch number *********** " + device.getBatchNumber());
