1003803e24b03723bc9eb0c5c81c4967bb11e317
Abhishek Sharma <abhisheksharma2091@gmail.com>
2018-02-26 17:44:47 +0530

    Revamping Files for File upload scenarios

--- src/main/java/com/mastercard/pts/integrated/issuing/domain/customer/helpdesk/HelpdeskGeneral.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/domain/customer/helpdesk/HelpdeskGeneral.java
@@ -1,16 +1,19 @@
 package com.mastercard.pts.integrated.issuing.domain.customer.helpdesk;
 
+import org.springframework.stereotype.Component;
+
 import com.mastercard.pts.integrated.issuing.domain.helpdesk.ProductType;
 import com.mastercard.pts.integrated.issuing.domain.provider.KeyValueProvider;
 import com.mastercard.pts.integrated.issuing.utils.ConstantData;
 
+@Component
 public class HelpdeskGeneral {
 	private static final String CS_SERVICE_CODE = "CS_SERVICE_CODE";
 	private static final String CURRENCY_SETUP_SERVICE_CODE = "CURRENCY_SETUP_SERVICE_CODE";
 	private static final String HD_CURRENCY_WITH_PRIORITY = "HD_CURRENCY_WITH_PRIORITY";
 	private static final String TRANSACTION_DETAILS = "TRANSACTION_DETAILS";
 	private static final String INITIAL_LOAD_TXN_DETAILS = "INITIAL_LOAD_TXN_DETAILS";
-	private static final String TRANSACTION_NOTE ="TRANSACTION_NOTE";
+	private static final String TRANSACTION_NOTE = "TRANSACTION_NOTE";
 	private String transactionNote;
 	private String initialLoadTxnDetails;
 	private String transactionDetails;
@@ -22,7 +25,7 @@ public class HelpdeskGeneral {
 	private String currencySetupServiceCode;
 	private String currencyWithPriority;
 	private String newWalletNumber;
-	
+
 	public static HelpdeskGeneral createWithProvider(KeyValueProvider provider) {
 		HelpdeskGeneral plan = new HelpdeskGeneral();
 		plan.setServiceCode(provider.getString(CS_SERVICE_CODE));
@@ -32,18 +35,18 @@ public class HelpdeskGeneral {
 		plan.setInitialLoadTxnDetails(provider.getString(INITIAL_LOAD_TXN_DETAILS));
 		plan.setNotes(ConstantData.GENERIC_DESCRIPTION);
 		plan.setProductType(ProductType.PREPAID);
-		plan.setProductType(provider.getString(TRANSACTION_NOTE));		
+		plan.setProductType(provider.getString(TRANSACTION_NOTE));
 		return plan;
 	}
-	
+
 	public String getNewWalletNumber() {
 		return newWalletNumber;
 	}
-	
+
 	public void setNewWalletNumber(String newWalletNumber) {
 		this.newWalletNumber = newWalletNumber;
 	}
-	
+
 	public String getInitialLoadTxnDetails() {
 		return initialLoadTxnDetails;
 	}
@@ -60,7 +63,6 @@ public class HelpdeskGeneral {
 		this.transactionDetails = transactionDetails;
 	}
 
-	
 	public String getTransactionNote() {
 		return transactionNote;
 	}
@@ -68,7 +70,7 @@ public class HelpdeskGeneral {
 	public void setTransactionNote(String transactionNote) {
 		this.transactionNote = transactionNote;
 	}
-	
+
 	public String getCurrencySetupServiceCode() {
 		return currencySetupServiceCode;
 	}
@@ -100,7 +102,7 @@ public class HelpdeskGeneral {
 	public void setNotes(String notes) {
 		this.notes = notes;
 	}
-	
+
 	public String getProductType() {
 		return productType;
 	}
@@ -127,8 +129,6 @@ public class HelpdeskGeneral {
 
 	@Override
 	public String toString() {
-		return "HelpdeskGeneral [productType=" + productType + ", cardPackId="
-				+ cardPackId + ", serviceCode=" + serviceCode + ", notes="
-				+ notes + ", deviceNumber=" + deviceNumber + "]";
+		return "HelpdeskGeneral [productType=" + productType + ", cardPackId=" + cardPackId + ", serviceCode=" + serviceCode + ", notes=" + notes + ", deviceNumber=" + deviceNumber + "]";
 	}
 }
