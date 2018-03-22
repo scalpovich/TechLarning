b681ee08838502a736262a85e89eb2f89015d464
Saikat Sengupta <saikat.sengupta@mastercard.com>
2018-03-22 14:31:30 +0530

    Add file converter from xls to csv

--- src/main/java/com/mastercard/pts/integrated/issuing/utils/FileConverter.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/utils/FileConverter.java
@@ -1,13 +1,20 @@
 package com.mastercard.pts.integrated.issuing.utils;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
@@ -17,28 +24,56 @@ import org.slf4j.LoggerFactory;
 
 public class FileConverter {
 	private static final Logger LOGGER = LoggerFactory.getLogger(FileConverter.class);
+	private String[] listOfEnvs = { "demo" };
+	private static final String CSV_EXTN = ".csv";
 
-	private void echoAsCSV(Sheet sheet) {
+	private void echoAsCSV(Sheet sheet, String env) {
 		// for vertical data
 		int startOfDataRow = 1;
-		List<String> headerValues = new ArrayList<>();
-		List<String> dataValues = new ArrayList<>();
+		List<String> headers = new ArrayList<>();
+		List<String> data = new ArrayList<>();
 		DataFormatter dataFormatter = new DataFormatter();
-		for (int i = startOfDataRow; i < sheet.getLastRowNum(); i++) {
-			headerValues.add(dataFormatter.formatCellValue(sheet.getRow(i).getCell(0)));
-			dataValues.add(dataFormatter.formatCellValue(sheet.getRow(i).getCell(1)));
+		for (int i = startOfDataRow; i <= sheet.getLastRowNum(); i++) {
+			Row row = sheet.getRow(i);
+			if (row != null) {
+				String headerCellVal = dataFormatter.formatCellValue(row.getCell(0));
+				String dataCellVal = dataFormatter.formatCellValue(row.getCell(1));
+				if (!headerCellVal.trim().isEmpty()) {
+					headers.add(headerCellVal);
+					data.add(dataCellVal);
+				}
+			}
 		}
 
-		LOGGER.info(headerValues.size() + " >> " + headerValues.toString());
-		LOGGER.info(dataValues.size() + " >> " + dataValues.toString());
+		LOGGER.info("{} {}", headers.size(), headers.toString());
+		LOGGER.info("{} {}", data.size(), data.toString());
+
+		File dir = new File("C" + ":" + File.separatorChar + "Temp" + File.separatorChar + env);
+		dir.mkdirs();
+		String fileAbsPath = dir.getAbsolutePath() + File.separatorChar + sheet.getSheetName() + CSV_EXTN;
+		writeToCSV(headers, data, fileAbsPath);
+	}
+
+	private void writeToCSV(List<String> headers, List<String> data, String filePath) {
+		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(filePath));
+				CSVPrinter printer = CSVFormat.EXCEL.withHeader(headers.toArray(new String[0])).print(writer);) {
+			printer.printRecord(data);
+			printer.flush();
+			LOGGER.info("CSV file created at: {}", filePath);
+		} catch (IOException e) {
+			LOGGER.error("IOException while creating CSV file at: {}", filePath, e);
+		}
 	}
 
 	public void convertExcelToCSV() throws IOException, InvalidFormatException {
-		try (InputStream in = new FileInputStream("./src/main/resources/config/demo/Data/TestData.xls");
-				Workbook wb = WorkbookFactory.create(in)) {
-			for (int i = 0; i < 1 /* wb.getNumberOfSheets() */; i++) {
-				LOGGER.info(wb.getSheetAt(i).getSheetName());
-				echoAsCSV(wb.getSheetAt(i));
+		for (String env : listOfEnvs) {
+			try (InputStream in = new FileInputStream(
+					String.format("./src/main/resources/config/%s/Data/TestData.xls", env));
+					Workbook wb = WorkbookFactory.create(in)) {
+				for (int i = 0; i < wb.getNumberOfSheets(); i++) {
+					LOGGER.info("sheetNo> {} sheetName> {}", i + 1, wb.getSheetAt(i).getSheetName());
+					echoAsCSV(wb.getSheetAt(i), env);
+				}
 			}
 		}
 	}
