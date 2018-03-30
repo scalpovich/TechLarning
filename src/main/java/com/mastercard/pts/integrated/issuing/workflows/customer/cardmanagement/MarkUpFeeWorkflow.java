da6606342b371750665c055364b6d719093b78c4
Mayur Sapre <e080534@mastercard.com>
2018-03-30 17:34:34 +0530

    MarkUpFeePlan Changes Updated

---
+++ src/main/java/com/mastercard/pts/integrated/issuing/workflows/customer/cardmanagement/MarkUpFeeWorkflow.java
@@ -0,0 +1,21 @@
+package com.mastercard.pts.integrated.issuing.workflows.customer.cardmanagement;
+
+import org.springframework.beans.factory.annotation.Autowired;	
+import org.springframework.stereotype.Component;
+
+import com.mastercard.pts.integrated.issuing.annotation.Workflow;
+import com.mastercard.pts.integrated.issuing.domain.customer.cardmanagement.MarkupFeePlan;
+import com.mastercard.pts.integrated.issuing.pages.customer.cardmanagement.MarkupFeePlanPage;
+import com.mastercard.pts.integrated.issuing.pages.navigation.Navigator;
+
+@Component
+public class MarkUpFeeWorkflow {
+	
+	@Autowired
+	Navigator navigator;
+
+	public void addMarkUpPlan(MarkupFeePlan plan) {
+		MarkupFeePlanPage markupplanpage = navigator.navigateToPage(MarkupFeePlanPage.class);
+		markupplanpage.createMarkupFeePlanPage(plan);		
+	}
+}
