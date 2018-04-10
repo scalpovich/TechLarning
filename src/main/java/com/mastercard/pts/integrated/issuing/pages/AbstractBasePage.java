a39aeca504594d543d9068ad2678eb47d42066c0
e076168 <e076168@GH-56L64M2.corp.mastercard.org>
2018-04-10 18:22:50 +0530

    DeviceSearch and update context

--- src/main/java/com/mastercard/pts/integrated/issuing/pages/AbstractBasePage.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/pages/AbstractBasePage.java
@@ -105,6 +105,8 @@ public abstract class AbstractBasePage extends AbstractPage {
 
 	private static final String EXCEPTION_MESSAGE = "Exception Message - {} ";
 
+	private static final String Device = null;
+
 	@Value("${default.wait.timeout_in_sec}")
 	private long timeoutInSec;
 
@@ -642,10 +644,7 @@ public abstract class AbstractBasePage extends AbstractPage {
 
 	protected void waitAndSearchForRecordToExist() {
 		waitAndSearchForRecordToAppear();
-		context.put(CreditConstants.DEVICE_NUMBER, deviceNumberFetch.getText());
-		Device device = context.get(ContextConstants.DEVICE);
-		device.setDeviceNumber(deviceNumberFetch.getText());
-		context.put(ContextConstants.DEVICE, device);
+		
 		selectFirstRecord();
 		clickProcessSelectedButton();
 	}
