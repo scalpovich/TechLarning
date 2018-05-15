d09adf411301fe6b1bb89afeade0eebaf4342a63
E079917 <ashutosh.nile@mastercard.com>
2018-05-15 18:58:39 +0530

    Updated code for Account Base Routing

--- src/main/java/com/mastercard/pts/integrated/issuing/steps/customer/cardmanagement/ChannelRoutingPlanStep.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/steps/customer/cardmanagement/ChannelRoutingPlanStep.java
@@ -6,6 +6,8 @@ import org.jbehave.core.annotations.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.mastercard.pts.integrated.issuing.context.ContextConstants;
+import com.mastercard.pts.integrated.issuing.context.TestContext;
 import com.mastercard.pts.integrated.issuing.domain.customer.cardmanagement.AccountRangeRoutingPlan;
 import com.mastercard.pts.integrated.issuing.domain.customer.cardmanagement.ChannelRoutingPlan;
 import com.mastercard.pts.integrated.issuing.workflows.customer.cardmanagement.AccountRangeRoutingFlows;
@@ -22,7 +24,11 @@ public class ChannelRoutingPlanStep  {
 	AccountRangeRoutingPlan accountRangeRoutingPlan;
 	@Autowired
 	AccountRangeRoutingFlows  accountRangeRoutingFlows;
-	
+	@Autowired
+	private TestContext context;
+	private final String channelNamePlan = "CBS1 [CBS1]" ;
+	private final String channelName = "CBS1" ;
+
 	@When("user creates channel Routing plan for $channelName channel and $interfaceName interface")
 	public void userCreatesChannelRouting(@Named("channelName") String channel,@Named("interfaceName") String interfaceType) {
 		channelRouting=ChannelRoutingPlan.channelRoutingPlanDataProvider();
@@ -32,14 +38,24 @@ public class ChannelRoutingPlanStep  {
 		channelRouting.setDescription(interfaceType);
 		channelRoutingFlows.addChannelRoutingPlan(channelRouting);
 	}
-	
-	
+
+
 	@When("user creates Acount Range Routing plan")
 	public void userCreatesChannelRouting() {
 		accountRangeRoutingPlan=AccountRangeRoutingPlan.channelRoutingPlanDataProvider();
 		accountRangeRoutingPlan.setChannelRoutingPlan(channelRouting.getPlanID());
 		accountRangeRoutingFlows.addChannelRoutingPlan(accountRangeRoutingPlan);
 	}
-	
-	
+
+	@When("user creates Acount Range Routing for $channelName channel")
+	public void userCreatesChannelRoutingForUserDefineChannel(String channelName) {
+		accountRangeRoutingPlan=AccountRangeRoutingPlan.channelRoutingPlanDataProvider(context.get(ContextConstants.DEVICE));
+		if(channelName.equals(this.channelName)){
+			accountRangeRoutingPlan.setChannelRoutingPlan(channelNamePlan);
+		}
+		accountRangeRoutingFlows.addChannelRoutingPlan(accountRangeRoutingPlan);
+	}
+
+
+
 }
