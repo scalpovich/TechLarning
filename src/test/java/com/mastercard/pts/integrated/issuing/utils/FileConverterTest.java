6d34f460ea364280b87ec8a6d0960e4411bb6d19
Saikat Sengupta <saikat.sengupta@mastercard.com>
2018-05-26 12:09:50 +0530

    Add INTERCHANGE info to all CSV files for demo, stagesa, auto, auto2 env

--- src/test/java/com/mastercard/pts/integrated/issuing/utils/FileConverterTest.java
+++ src/test/java/com/mastercard/pts/integrated/issuing/utils/FileConverterTest.java
@@ -1,8 +1,15 @@
 package com.mastercard.pts.integrated.issuing.utils;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -17,8 +24,8 @@ public class FileConverterTest {
 	@Ignore
 	@Test
 	public void convertExcelToCsv() throws IOException, InvalidFormatException {
-		String[] listOfEnvs = { "demo", "automation2", "stageSA", /*automation*/ };
-		String[] dataFiles = { "TestData/TestData.xlsx", /*"Data/TestData.xls"*/ };
+		String[] listOfEnvs = { "demo", "automation2", "stageSA", /* automation */ };
+		String[] dataFiles = { "TestData/TestData.xlsx", /* "Data/TestData.xls" */ };
 		for (String env : listOfEnvs) {
 			for (String dataFile : dataFiles) {
 				try (InputStream in = new FileInputStream(
@@ -32,6 +39,70 @@ public class FileConverterTest {
 				}
 			}
 		}
+	}
+
+	@Test
+	public void addNewColumnToAllCSV() {
+		final String COLUMN_TO_ADD = "INTERCHANGE";
+		final String DELIMITER = ",";
+		final String VISA = "VISA [01]";
+		final String MC = "MASTERCARD [02]";
+		final String[] VISA_PART_FILENAMES = { "_v_", "vts", "visa" };
+		String[] listOfEnvs = { "demo", "automation2", "stageSA", "automation" };
+
+		for (String env : listOfEnvs) {
+
+			File[] csvFiles = new File(FileConverter.CONFIG_PATH + env + File.separatorChar + "Data")
+					.listFiles((dir, name) -> {
+						return name.toLowerCase().endsWith(".csv");
+					});
+
+			Stream.of(csvFiles).forEach(csvFile -> {
+				try {
+					List<String> lines = Files.readAllLines(Paths.get(csvFile.getAbsolutePath()));
+					String header = lines.get(0);
+					if (!header.contains(COLUMN_TO_ADD)) {
+						List<String> data = new ArrayList<>();
+						header = header + DELIMITER + COLUMN_TO_ADD;
+						data.add(header);
+
+						String val = MC;
+						if (fileNameMatches(VISA_PART_FILENAMES, csvFile.getName())) {
+							val = VISA;
+						}
+						for (int i = 1; i < lines.size(); i++) {
+							String row = lines.get(i);
+							row = row + DELIMITER + val;
+							data.add(row);
+						}
+						writeToFile(data, csvFile);
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			});
+		}
+	}
+
+	private void writeToFile(List<String> data, File csvFile) {
+		try (FileWriter writer = new FileWriter(csvFile)) {
+			for (String row : data) {
+				writer.write(row + System.lineSeparator());
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private boolean fileNameMatches(String[] array, String item) {
+		for (String search : array) {
+			if (item.contains(search)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
