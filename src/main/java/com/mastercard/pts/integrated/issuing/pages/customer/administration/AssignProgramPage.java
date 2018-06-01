72ae8361c5419e1f596fababc6a13c68f3b53e77
E079917 <ashutosh.nile@mastercard.com>
2018-06-01 19:41:08 +0530

    Code for service code association with program

---
+++ src/main/java/com/mastercard/pts/integrated/issuing/pages/customer/administration/AssignProgramPage.java
@@ -0,0 +1,48 @@
+package com.mastercard.pts.integrated.issuing.pages.customer.administration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import com.mastercard.pts.integrated.issuing.domain.customer.cardmanagement.Program;
+import com.mastercard.pts.integrated.issuing.pages.AbstractBasePage;
+import com.mastercard.pts.integrated.issuing.pages.navigation.annotation.Navigation;
+import com.mastercard.pts.integrated.issuing.utils.WebElementUtils;
+import com.mastercard.testing.mtaf.bindings.element.MCWebElement;
+import com.mastercard.testing.mtaf.bindings.element.ElementsBase.FindBy;
+import com.mastercard.testing.mtaf.bindings.page.PageElement;
+
+@Component
+@Navigation(tabTitle = AdministrationNav.TAB_ADMINISTRATION, treeMenuItems = {
+		AdministrationNav.L1_SETUP,
+		AdministrationNav.L2_HELPDESK_PRIVILEGES,
+		AdministrationNav.L3_ASSIGN_PROGRAM
+})
+public class AssignProgramPage extends AbstractBasePage {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(AssignProductPage.class);
+
+	@PageElement(findBy = FindBy.NAME, valueToFind = "programCode:input:dropdowncomponent")
+	private MCWebElement programDDwn;
+
+	@PageElement(findBy = FindBy.CSS, valueToFind = "select[name^=planPalette]")
+	private MCWebElement availableServiceCodeLstBx;
+
+	@PageElement(findBy = FindBy.CSS, valueToFind = "button.add")
+	private MCWebElement addServiceCodeBtn;
+
+	public void addServiceCodeToProgram(Program program) {
+		logger.info("Program Code {}", program.getProgramCodeDevice());
+		clickAddNewButton();
+		runWithinPopup(
+				"Add SR Visibility at Program Level",
+				() -> {
+					WebElementUtils.selectDDByVisibleText(programDDwn,  program.getProgramCodeDevice());
+					WebElementUtils .selectAllOptionsInListBox(availableServiceCodeLstBx);
+					addServiceCodeBtn.click();
+					clickSaveButton();
+					verifyNoErrors();
+				});
+		verifyOperationStatus();
+	}
+}
---
+++ src/main/java/com/mastercard/pts/integrated/issuing/steps/customer/administration/AssignProgramStep.java
@@ -0,0 +1,27 @@
+package com.mastercard.pts.integrated.issuing.steps.customer.administration;
+
+import org.jbehave.core.annotations.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mastercard.pts.integrated.issuing.context.ContextConstants;
+import com.mastercard.pts.integrated.issuing.context.TestContext;
+import com.mastercard.pts.integrated.issuing.domain.customer.cardmanagement.Program;
+import com.mastercard.pts.integrated.issuing.workflows.customer.administration.AssignProgramWorkflow;
+
+@Component
+public class AssignProgramStep {
+	
+	@Autowired
+	AssignProgramWorkflow assignProgramWorkflow;
+	
+	@Autowired
+	private TestContext context;
+	
+
+	@When("user assign service code to program")
+	public void assignServiceCodeToProgram(){
+		Program	program = context.get(ContextConstants.PROGRAM);
+		assignProgramWorkflow.assignServiceCodeToProgram(program);
+	}
+}
