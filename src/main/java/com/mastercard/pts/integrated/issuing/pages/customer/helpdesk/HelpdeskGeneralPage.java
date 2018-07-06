3d263e6ffbc5fa39432ae46eca1de8aebb44de96
Mayur Sapre <mayur.sapre@mastercard.com>
2018-07-06 14:52:18 +0530

    Device Number Change

--- src/main/java/com/mastercard/pts/integrated/issuing/pages/customer/helpdesk/HelpdeskGeneralPage.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/pages/customer/helpdesk/HelpdeskGeneralPage.java
@@ -518,9 +518,9 @@ public class HelpdeskGeneralPage extends AbstractBasePage {
     }
 
 	public String getDeviceStatus(Device device) {
-		logger.info("Fetching information for : {}","5897651997732016");
-		//WebElementUtils.selectDropDownByVisibleText(productTypeSearchDDwn, device.getAppliedForProduct());
-		WebElementUtils.enterText(deviceNumberSearchTxt,"5897651997732016");
+		logger.info("Fetching information for : {}",device.getDeviceNumber());
+		WebElementUtils.selectDropDownByVisibleText(productTypeSearchDDwn, device.getAppliedForProduct());
+		WebElementUtils.enterText(deviceNumberSearchTxt,device.getDeviceNumber());
 		clickSearchButton();
 		SimulatorUtilities.wait(5000);//this to wait till the table gets loaded
 		return getFirstRecordCellTextByColumnName(COLUMN_STATUS);
