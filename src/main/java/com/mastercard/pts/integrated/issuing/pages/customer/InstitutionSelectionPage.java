9691673240da5ada60871139f15ce46313421e73
MASTERCARD\E075551 <nitin.kumar@mastercard.com>
2018-07-11 10:49:54 +0530

    handled Instiution Selection

--- src/main/java/com/mastercard/pts/integrated/issuing/pages/customer/InstitutionSelectionPage.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/pages/customer/InstitutionSelectionPage.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import com.mastercard.pts.integrated.issuing.domain.customer.cardmanagement.InstitutionSelection;
 import com.mastercard.pts.integrated.issuing.pages.AbstractBasePage;
 import com.mastercard.pts.integrated.issuing.utils.WebElementUtils;
+import com.mastercard.pts.integrated.issuing.utils.simulator.SimulatorUtilities;
 import com.mastercard.testing.mtaf.bindings.element.ElementsBase.FindBy;
 import com.mastercard.testing.mtaf.bindings.element.MCWebElement;
 import com.mastercard.testing.mtaf.bindings.page.PageElement;
@@ -21,13 +22,12 @@ import com.mastercard.testing.mtaf.bindings.page.PageElement;
  */
 @Component
 public class InstitutionSelectionPage extends AbstractBasePage {
-	
+
 	private static final String OPTION_SELECT_ONE = "Select One";
-	
+
 	@PageElement(findBy = FindBy.CSS, valueToFind = "select[name^=institutionCode]")
 	private MCWebElement institutionSelect;
-	
-	
+
 	@PageElement(findBy = FindBy.NAME, valueToFind = "institutionCode:input:dropdowncomponent")
 	private MCWebElement institution;
 
@@ -50,8 +50,8 @@ public class InstitutionSelectionPage extends AbstractBasePage {
 
 	public void selectInstitution(String... optionName) {
 		if (optionName.length == 0) {
-			selectByVisibleText(institution, InstitutionSelection.IntitutionDataProvider()
-					.getInstiution());
+			selectByVisibleText(institution, InstitutionSelection
+					.IntitutionDataProvider().getInstiution());
 		} else {
 			selectByVisibleText(institution, Arrays.toString(optionName));
 		}
@@ -60,44 +60,52 @@ public class InstitutionSelectionPage extends AbstractBasePage {
 
 	public void selectAdminInstitution(String... optionName) {
 		if (optionName.length == 0) {
-			selectByVisibleText(institution, InstitutionSelection.IntitutionDataProvider()
-					.getAdminInstiutionName());
-		} else {
-			selectByVisibleText(institution, Arrays.toString(optionName));
-		}
-		confirmButton.click();
-	}
-	
-	public void selectBankAdminInstitution(String... optionName) {
-		if (optionName.length == 0) {
-			selectByVisibleText(institution, InstitutionSelection.IntitutionDataProvider()
-					.getBankInstitution());
+			selectByVisibleText(institution, InstitutionSelection
+					.IntitutionDataProvider().getAdminInstiutionName());
 		} else {
 			selectByVisibleText(institution, Arrays.toString(optionName));
 		}
 		confirmButton.click();
 	}
 
+	public void selectBankAdminInstitution(String... optionName) {
+		if (optionName.length == 0) {
+			selectByVisibleText(institution, InstitutionSelection
+					.IntitutionDataProvider().getBankInstitution());
+		} else {
+			selectByVisibleText(institution, Arrays.toString(optionName));
+		}
+		confirmButton.click();
+	}
 
 	@Override
 	protected List<ExpectedCondition<WebElement>> isLoadedConditions() {
-		return Arrays.asList(
-				WebElementUtils.visibilityOf(institutionSelect),
+		return Arrays.asList(WebElementUtils.visibilityOf(institutionSelect),
 				WebElementUtils.elementToBeClickable(confirmButton));
 	}
 
 	public List<String> getAvailableInstitutions() {
 		return institutionSelect.getSelect().getOptions().stream()
-				.map(WebElement::getText).filter(text -> !text.equals(OPTION_SELECT_ONE))
+				.map(WebElement::getText)
+				.filter(text -> !text.equals(OPTION_SELECT_ONE))
 				.collect(Collectors.toList());
 	}
-	
+
 	public String getSelectedInstitution() {
 		return institutionSelect.getSelect().getFirstSelectedOption().getText();
 	}
-	
-	
+
 	public void clickConfirm() {
 		confirmButton.click();
 	}
+
+	public boolean checkSessionExpired() {
+		SimulatorUtilities.wait(5000);
+		if (Elements("//ul[@class='feedbackPanel']//span").size() > 0) {
+			Element("//a[contains(text(),'Click here to login')]").click();
+			return true;
+		}
+		return false;
+	}
+
 }
