9d335baee25a181997fddc3f3f428ea617422cb5
Ravindra Thakare <e074060@mastercard.com>
2018-07-18 11:19:50 +0530

    removed comments

--- src/main/java/com/mastercard/pts/integrated/issuing/workflows/customer/cardmanagement/TransactionLimitPlanFlows.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/workflows/customer/cardmanagement/TransactionLimitPlanFlows.java
@@ -37,10 +37,8 @@ public class TransactionLimitPlanFlows extends MenuFlows {
 	}
 	
 	public void clickSave(){
-	//	runWithinPopup("Add Transaction Limit Plan", () -> {
 		WebElementUtils.scrollDown(driver(), 0, 300);
 		pageToPass.clickSaveButton();
-	//	});
 	}
 	
 	public void addDetails(TransactionLimitPlan transactionLimitPlan) {
