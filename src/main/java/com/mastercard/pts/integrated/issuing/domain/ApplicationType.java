d34a808a91d2f81dfe8cc6ca87785fccb8175817
Tuptewar <Pavan.Tuptewar@mastercard.com>
2018-07-26 18:53:09 +0530

    ApplicationType

--- src/main/java/com/mastercard/pts/integrated/issuing/domain/ApplicationType.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/domain/ApplicationType.java
@@ -5,4 +5,8 @@ public class ApplicationType {
 	public static final String PRIMARY_DEVICE = "Primary";
 	public static final String SUPPLEMENTARY_DEVICE ="Supplementary";
 	public static final String ADD_ON_DEVICE = "Add-on";
+	
+	private ApplicationType(){
+		
+	}
 }
