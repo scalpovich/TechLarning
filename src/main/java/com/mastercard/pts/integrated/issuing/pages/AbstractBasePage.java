895483eb4253bb4808307ff8dc71b3b1c7a3aa48
Abhishek Sharma <abhisheksharma2091@gmail.com>
2018-07-27 16:44:33 +0530

    Removing unchnaged file

--- src/main/java/com/mastercard/pts/integrated/issuing/pages/AbstractBasePage.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/pages/AbstractBasePage.java
@@ -1760,7 +1760,7 @@ public abstract class AbstractBasePage extends AbstractPage {
 		CustomUtils.ThreadDotSleep(500);
 		getFinder().getWebDriver().findElement(By.xpath(ins)).click();
 	}
-	
+
 	public void deleteExistingRecord(String parameter) {
 		for (int i = 0; i < firstElementOfTable.getElements().size(); i++) {
 			if (firstElementOfTable.getElements().get(i).getText().equals(parameter)) {
