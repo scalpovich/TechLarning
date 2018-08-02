a377eda88aa1718a34a626c110a57e75e66b1d6a
Abhishek Sharma <abhisheksharma2091@gmail.com>
2018-08-02 16:27:28 +0530

    Adding deleted files

---
+++ src/main/java/com/mastercard/pts/integrated/issuing/steps/customer/cardmanagement/ManualAuthorizationSteps.java
@@ -0,0 +1,48 @@
+package com.mastercard.pts.integrated.issuing.steps.customer.cardmanagement;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mastercard.pts.integrated.issuing.context.ContextConstants;
+import com.mastercard.pts.integrated.issuing.context.TestContext;
+import com.mastercard.pts.integrated.issuing.domain.customer.cardmanagement.AuthorizationRequest;
+import com.mastercard.pts.integrated.issuing.domain.customer.cardmanagement.Device;
+import com.mastercard.pts.integrated.issuing.domain.provider.KeyValueProvider;
+import com.mastercard.pts.integrated.issuing.workflows.customer.cardmanagement.ManualAuthorizationWorkflow;
+
+@Component
+public class ManualAuthorizationSteps {
+
+	@Autowired
+	private TestContext context;
+
+	@Autowired
+	private KeyValueProvider provider;
+	
+	@Autowired
+	private ManualAuthorizationWorkflow manualAuthorizationWorkflow;
+	
+	private String successMessage;
+
+	@Given("user raises an authorization request")
+	@When("user raises an authorization request")
+	@Then("user raises an authorization request")
+	public void whenUserRaisesAnAuthorizationRequest(){
+		AuthorizationRequest request = AuthorizationRequest.createWithProvider(provider);
+		Device device = context.get(ContextConstants.DEVICE);
+		request.setDeviceNumber(device.getDeviceNumber());
+		successMessage = manualAuthorizationWorkflow.authorizeDevice(request);
+	}
+	
+	@Then("status of request is \"approved\"")
+	public void thenStatusOfRequestIsapproved(){
+		 assertThat("Authorization is successful", successMessage, containsString("Authorization is successful"));
+
+	}
+}
