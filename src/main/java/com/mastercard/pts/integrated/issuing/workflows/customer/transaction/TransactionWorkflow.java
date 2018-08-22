7dd7053853e51ec5f82fb8fe1f9da13d3fde19f9
Ravindra Thakare <e074060@mastercard.com>
2018-08-22 19:11:41 +0530

    minor change

--- src/main/java/com/mastercard/pts/integrated/issuing/workflows/customer/transaction/TransactionWorkflow.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/workflows/customer/transaction/TransactionWorkflow.java
@@ -647,7 +647,7 @@ public class TransactionWorkflow extends SimulatorUtilities {
 		action.moveToElement(winiumDriver.findElementByName("012 - Date And Time, Local Transaction")).doubleClick().build().perform();
 		wait(1000);
 		activateEditField();
-		winiumDriver.findElementByName(EDIT_SUBFIELD_VALUE).getText();
+		winiumDriver.findElementByName(EDIT_SUBFIELD_VALUE).click();
 		setText("");
 		setText(date);
 		wait(1000);
