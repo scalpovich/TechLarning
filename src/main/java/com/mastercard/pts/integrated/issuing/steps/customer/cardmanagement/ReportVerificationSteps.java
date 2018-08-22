2510812316cfec7ea6212c3d477dfee26f03f6a1
MASTERCARD\E075551 <nitin.kumar@mastercard.com>
2018-08-22 17:28:46 +0530

    temp

--- src/main/java/com/mastercard/pts/integrated/issuing/steps/customer/cardmanagement/ReportVerificationSteps.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/steps/customer/cardmanagement/ReportVerificationSteps.java
@@ -44,7 +44,8 @@ public class ReportVerificationSteps {
 		catch(ArrayIndexOutOfBoundsException e){
 			logger.info("No Report Type is present here!!");
 		}
-		report.setDeviceNumber(device.getDeviceNumber());
+		//report.setDeviceNumber(device.getDeviceNumber());
+		report.setDeviceNumber("5383162544459112");
 		report.setUsername(context.get(UserManagementSteps.USERNAME));
 		for(String field : reportFields.split(",")){
 			report.setFieldToValidate(field, context.get(ConstantData.fromShortName(field)));
