3e31b21a69e0cbce567e92fc6a81473c3e252034
Abhishek Sharma <abhisheksharma2091@gmail.com>
2018-09-04 12:54:15 +0530

    Updating code

---
+++ src/main/java/com/mastercard/pts/integrated/issuing/workflows/customer/cardmanagement/CreditCardPaymentWorkFlows.java
@@ -0,0 +1,43 @@
+package com.mastercard.pts.integrated.issuing.workflows.customer.cardmanagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mastercard.pts.integrated.issuing.domain.customer.cardmanagement.Payment;
+import com.mastercard.pts.integrated.issuing.pages.customer.cardmanagement.CashPage;
+import com.mastercard.pts.integrated.issuing.pages.customer.cardmanagement.LocalChequePage;
+import com.mastercard.pts.integrated.issuing.pages.customer.cardmanagement.OutstationChequeCollectionPage;
+import com.mastercard.pts.integrated.issuing.pages.customer.cardmanagement.OutstationChequeProcessingPage;
+import com.mastercard.pts.integrated.issuing.pages.navigation.Navigator;
+
+@Component
+public class CreditCardPaymentWorkFlows {
+	
+	@Autowired
+	private Navigator navigator;
+	
+	LocalChequePage localChqPage;	
+	CashPage cashPage;
+	OutstationChequeCollectionPage outstationChequeCollectionPage;
+	OutstationChequeProcessingPage outstationChequeProcessingPage;
+	
+	public void makeLocalChequePayment(Payment local){
+		localChqPage = navigator.navigateToPage(LocalChequePage.class);
+		localChqPage.addLocalChequePayment(local);
+	}
+	
+	public void makeCashPayment(Payment cashPay){
+		cashPage = navigator.navigateToPage(CashPage.class);
+		cashPage.performCashPayment(cashPay);
+	}
+
+	public void makeOutStationCollectionPayment(Payment outStationCollection){
+		outstationChequeCollectionPage = navigator.navigateToPage(OutstationChequeCollectionPage.class);
+		outstationChequeCollectionPage.performOutStationCollectionPayment(outStationCollection);
+	}
+	
+	public boolean checkOutStationProcessing(){
+		outstationChequeProcessingPage=navigator.navigateToPage(OutstationChequeProcessingPage.class);
+		return outstationChequeProcessingPage.isTextAvailableinTable();
+	}
+}
