b1bb939c05625689e60e9726f32476f7a7fa3c75
Sapre, Mayur <Mayur.Sapre@mastercard.com>
2018-09-06 02:42:24 -0500

    ContextConstants.java edited online with Bitbucket

--- src/main/java/com/mastercard/pts/integrated/issuing/context/ContextConstants.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/context/ContextConstants.java
@@ -70,7 +70,6 @@ public class ContextConstants {
 	
 	public static final String DOMESTIC = "domestic";
 
-	
 	public static final String DEVICE_RANGE = "DEVICE_RANGE";
 	
 	public static final String ALL_DEVICE_NUMBERS = "ALL_DEVICE_NUMBERS";
