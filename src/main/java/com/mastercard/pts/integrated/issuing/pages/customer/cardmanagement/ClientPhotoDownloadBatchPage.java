ae07df2e6d0a0cc1fbf932671421c2ea783015a9
Vaibhav Revanwar <vaibhav.revanwar@mastercard.com>
2018-09-26 11:50:41 +0530

    Search for record method modified in client photo download batch

--- src/main/java/com/mastercard/pts/integrated/issuing/pages/customer/cardmanagement/ClientPhotoDownloadBatchPage.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/pages/customer/cardmanagement/ClientPhotoDownloadBatchPage.java
@@ -50,6 +50,7 @@ public class ClientPhotoDownloadBatchPage extends AbstractBasePage {
 		enterValueinTextBox(applicationNumberTxt,device.getApplicationNumber());
 		clickSearchButton();
 		SimulatorUtilities.wait(2000);
+		waitAndSearchForRecordToAppear();
 		if(isNoRecordsFoundInTable())
 		{a[0]=false; a[1]=false;}
 		else
