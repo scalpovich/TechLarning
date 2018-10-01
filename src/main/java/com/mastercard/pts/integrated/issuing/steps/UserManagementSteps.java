659276c5d4f5850b6963ca39b9bc827f25028a0f
Sunil Gupta <sunil.gupta@mastercard.com>
2018-10-01 10:30:02 +0530

    update time

--- src/main/java/com/mastercard/pts/integrated/issuing/steps/UserManagementSteps.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/steps/UserManagementSteps.java
@@ -331,6 +331,6 @@ public class UserManagementSteps {
 	@When("user wait for $time to perform certain activity")
 	public void whenUserWaitForSomeMinutesToPerformCertainActivity(String time)
 	{
-		SimulatorUtilities.wait(425000);
+		SimulatorUtilities.wait(375000);
 	}
 }
