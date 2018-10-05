8f734ff3b3ef4592470fef30fb9f4aa4721a21f3
Gaikwad <Trupti.Gaikwad@mastercard.com>
2018-10-05 12:21:29 +0530

    Added wait for stage execution

--- src/main/java/com/mastercard/pts/integrated/issuing/pages/customer/cardmanagement/AuthorizationRequestPage.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/pages/customer/cardmanagement/AuthorizationRequestPage.java
@@ -14,6 +14,7 @@ import com.mastercard.pts.integrated.issuing.pages.AbstractBasePage;
 import com.mastercard.pts.integrated.issuing.pages.navigation.annotation.Navigation;
 import com.mastercard.pts.integrated.issuing.utils.Constants;
 import com.mastercard.pts.integrated.issuing.utils.WebElementUtils;
+import com.mastercard.pts.integrated.issuing.utils.simulator.SimulatorUtilities;
 import com.mastercard.testing.mtaf.bindings.element.ElementsBase.FindBy;
 import com.mastercard.testing.mtaf.bindings.element.MCWebElement;
 import com.mastercard.testing.mtaf.bindings.page.PageElement;
@@ -57,6 +58,7 @@ public class AuthorizationRequestPage extends AbstractBasePage{
 			WebElementUtils.enterText(transactionAmountTxt, request.getTransactionAmount());
 			WebElementUtils.enterText(memoTxt, request.getMemo());
 			clickSaveButton();
+			SimulatorUtilities.wait(4000);
 			statusMessage = getMessageFromFeedbackPanel();
 			logger.info("Authorization Status: " + statusMessage);
 			clickOkButton();
