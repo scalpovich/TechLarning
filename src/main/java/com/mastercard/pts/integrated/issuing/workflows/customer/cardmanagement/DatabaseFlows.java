7f80ef9d55d728dc60d1a4f709f4f0e7f9f1b838
Sengupta, Saikat <Saikat.Sengupta@mastercard.com>
2018-10-30 02:07:59 -0500

    DatabaseFlows.java edited online with Bitbucket

--- src/main/java/com/mastercard/pts/integrated/issuing/workflows/customer/cardmanagement/DatabaseFlows.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/workflows/customer/cardmanagement/DatabaseFlows.java
@@ -27,7 +27,7 @@ public class DatabaseFlows {
 	@Autowired
 	private DBUtility dbUtil;	
 	
-	@Value("${institution}")
+	//@Value("${institution}")
 	private String institution;
 
 	public void updateInstituteDateToFirstOfNextMonth(String date)
