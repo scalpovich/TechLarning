0570cc2911ae8044939d162c1c1b5ddc896b4f06
e076168 <e076168@GH-56L64M2.corp.mastercard.org>
2018-12-28 14:51:33 +0530

    Pin-set

--- src/main/java/com/mastercard/pts/integrated/issuing/pages/cardholder/pinset/DevicePinSetPage.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/pages/cardholder/pinset/DevicePinSetPage.java
@@ -1,52 +1,119 @@
 package com.mastercard.pts.integrated.issuing.pages.cardholder.pinset;
 
-import java.util.List;
+import java.time.LocalDate;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import com.google.common.base.Strings;
+import com.mastercard.pts.integrated.issuing.domain.cardholder.CardHolderPinSet;
+import com.mastercard.pts.integrated.issuing.domain.customer.cardmanagement.Device;
 import com.mastercard.pts.integrated.issuing.pages.AbstractBasePage;
-import com.mastercard.pts.integrated.issuing.pages.cardholder.enquiry.TransactionsPage;
 import com.mastercard.pts.integrated.issuing.pages.navigation.annotation.Navigation;
+import com.mastercard.pts.integrated.issuing.utils.WebElementUtils;
+import com.mastercard.pts.integrated.issuing.utils.simulator.SimulatorUtilities;
 import com.mastercard.testing.mtaf.bindings.element.ElementsBase.FindBy;
 import com.mastercard.testing.mtaf.bindings.element.MCWebElement;
 import com.mastercard.testing.mtaf.bindings.page.PageElement;
-import com.mastercard.pts.integrated.issuing.domain.cardholder.CardHolderPinSet;
 
 @Component 
 @Navigation(tabTitle = CardHolderPinSet.TAB_PINSET, treeMenuItems = {CardHolderPinSet.PIN_SET})
 public class DevicePinSetPage extends AbstractBasePage{	
 	
-	private static final Logger logger = LoggerFactory.getLogger(TransactionsPage.class);
+	@PageElement(findBy = FindBy.NAME, valueToFind = "expiryDate")
+	MCWebElement cardExpDateTxt;
 	
-	private String deviceGrid = "//form[@id='myForm']/table[2]/tbody/child::tr";
+	@PageElement(findBy = FindBy.NAME, valueToFind = "cvv")
+	MCWebElement cvv2CVC2Txt;
 	
-	@PageElement(findBy = FindBy.X_PATH, valueToFind="//form[@id='myForm']/table[2]/tbody/tr")
-	private MCWebElement devicesTable;
+	@PageElement(findBy = FindBy.NAME, valueToFind = "legalid")
+	MCWebElement documentTypeDDwn;
 	
-	@PageElement(findBy = FindBy.X_PATH, valueToFind="//div[@class='dataview-div']/table/tbody/tr[2]")
-	private MCWebElement deviceTableRow;
+	@PageElement(findBy = FindBy.NAME, valueToFind = "legalidtype")
+	MCWebElement documentNumberTxt;
 	
-	@PageElement(findBy = FindBy.X_PATH, valueToFind="//div[@class='dataview-div']/table/tbody/tr[2]")
-	private MCWebElement selectDeviceBtn;
+	@PageElement(findBy = FindBy.NAME, valueToFind = "issuanceplace")
+	MCWebElement documentPlaceTxt;	
 	
+	@PageElement(findBy = FindBy.ID, valueToFind = "legalexpdate")
+	MCWebElement dtPkrDocumentExp;
 	
-	//Pending script for Device Pin Set
-	public void selectDeviceForPinSet(String cardNumber){		
-		List<WebElement> rowsList = Elements(deviceGrid);
-		
-		for(int index =2; index <= Elements(deviceGrid).size() ; index++){
-			if(rowsList.get(index).findElement((By) deviceTableRow).getAttribute("id").equals("5206797198380018")){				
-				getDeviceId(rowsList.get(index).findElement((By) deviceTableRow));
-			}
+	@PageElement(findBy = FindBy.ID, valueToFind="mpts_cardHolderPortal_button_submit")
+	private MCWebElement submitBtn;
+	
+	@PageElement(findBy = FindBy.X_PATH, valueToFind="//*[@class='sectionHead']/td/../following-sibling::tr[1]/td")
+	private MCWebElement lablResponse;
+	
+	@PageElement(findBy = FindBy.X_PATH, valueToFind = "//*[@*='birthDate']/../img")
+	private MCWebElement birthDateDPkr;
+	
+	@PageElement(findBy = FindBy.NAME, valueToFind = "pinNew")
+	private MCWebElement newPinTxt;
+	
+	@PageElement(findBy = FindBy.NAME, valueToFind = "pinNew2")
+	private MCWebElement confirmTxt;
+	
+	@PageElement(findBy = FindBy.CLASS, valueToFind = "keypad-special keypad-close")
+	private MCWebElement pinConfirmBtn;
+	
+	@PageElement(findBy = FindBy.CLASS, valueToFind = "ui-datepicker-year")
+	private MCWebElement yearDropDdn;
+	
+	@PageElement(findBy = FindBy.CLASS, valueToFind = "ui-datepicker-month")
+	private MCWebElement monthDropDdn;	
+	
+	private static final Logger logger = LoggerFactory.getLogger(DevicePinSetPage.class);
+	
+	public String setPinForDevice(Device device){
+		logger.info("Device Number: {}", device.getDeviceNumber());
+		String locator = String.format("//*[@class='dataview-div']//*[@id='%s']//*[@type='submit']", device.getDeviceNumber());
+		Element(locator).click();
+		waitForLoaderToDisappear();
+		enterText(cardExpDateTxt, "1022");
+		logger.info("CVV2 or CVC2 pin: {}", device.getCvv2Data());
+		enterText(cvv2CVC2Txt,device.getCvv2Data());		
+		logger.info("Client DOB: {}", device.getClientDetails().getBirthDate());
+		setDate(device.getClientDetails().getBirthDate());		
+		WebElementUtils.selectDropDownByIndex(documentTypeDDwn, 1);
+		logger.info("Document ID: {}", device.getLegalID());
+		enterText(documentNumberTxt, device.getLegalID());
+		clickWhenClickable(submitBtn);
+		waitForLoaderToDisappear();	
+		setNewPin("1234");
+		clickWhenClickable(submitBtn);
+		return lablResponse.getText();
+	}
+
+	public  void setNewPin(String newPin){
+		if(!Strings.isNullOrEmpty(newPin)){
+			char[] pinArry = newPin.toCharArray();
+				
+				clickWhenClickable(newPinTxt);
+				for(char pin : pinArry){
+					String locator = String.format("//*[@*='keypad-key' and text()='%c']", pin);
+					Element(locator).click();
+				}
+				clickWhenClickable(pinConfirmBtn);
+				SimulatorUtilities.wait(2000);
+				
+				clickWhenClickable(confirmTxt);
+				for(char pin : pinArry){
+					String locator = String.format("//*[@*='keypad-key' and text()='%c']", pin);
+					Element(locator).click();
+				}
+				clickWhenClickable(pinConfirmBtn);
+				SimulatorUtilities.wait(2000);
 		}
 	}
+
 	
-	public String getDeviceId(WebElement webElement){
-//		getFinder().getWebDriver().
-		return null;
+	public void setDate(LocalDate date){		
+		asWebElement(birthDateDPkr).click();
+		selectByVisibleText(yearDropDdn, String.valueOf(date.getYear()));
+		SelectDropDownByValue(monthDropDdn, String.valueOf(date.getMonthValue()-1));
+		String locator = String.format("//*[@data-handler='selectDay']/a[text()=%d]", date.getDayOfMonth());
+		driver().findElement(By.xpath(locator)).click();		
 	}
-	
 }
