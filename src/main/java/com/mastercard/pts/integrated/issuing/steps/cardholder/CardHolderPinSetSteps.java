b0455d9f6d51df7de4b30492d9ab19d1b8b7b882
e076168 <e076168@GH-56L64M2.corp.mastercard.org>
2018-12-28 14:55:50 +0530

    Pin set fixes

--- src/main/java/com/mastercard/pts/integrated/issuing/steps/cardholder/CardHolderPinSetSteps.java
+++ src/main/java/com/mastercard/pts/integrated/issuing/steps/cardholder/CardHolderPinSetSteps.java
@@ -1,10 +1,15 @@
 package com.mastercard.pts.integrated.issuing.steps.cardholder;
 
+import static junit.framework.Assert.assertTrue;
+
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.mastercard.pts.integrated.issuing.context.ContextConstants;
+import com.mastercard.pts.integrated.issuing.context.TestContext;
+import com.mastercard.pts.integrated.issuing.domain.customer.cardmanagement.Device;
 import com.mastercard.pts.integrated.issuing.steps.AbstractBaseSteps;
 import com.mastercard.pts.integrated.issuing.workflows.cardholder.pinset.CardHolderPinSetWorkflow;
 
@@ -14,9 +19,13 @@ public class CardHolderPinSetSteps extends AbstractBaseSteps{
 	@Autowired
 	CardHolderPinSetWorkflow cardHolderPinSetWorkflow;
 	
+	@Autowired
+	private TestContext context;
+	
 	@Then ("PIN set request for card")
 	@When ("PIN set request for card")
-	public void requestPinSetForDevice(){		
-		cardHolderPinSetWorkflow.selectDeviceFromDeviceLst("DeviceForPinSet");
+	public void requestPinSetForDevice(){
+		Device device = context.get(ContextConstants.DEVICE);
+		assertTrue("Error while creating PIN set request", cardHolderPinSetWorkflow.setPinRequest(device).contains("Your transaction is successful"));
 	}
 }
